525898f7677187fbfea65effdfca06365206b0ff
Harsh Raj Jha <84229424+Hrjha30@users.noreply.github.com>
2021-10-19 15:10:21 +0530

    Create Enter_and_display_1-D_array.cpp

---
+++ Enter_and_display_1-D_array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{int i,a[3];
+cout<<"Enter the Elements : ";
+for(i=0;i<=2;i++)
+{
+cin>>a[i];
+}
+cout<<"\nThe entered elements are : ";
+for(i=0;i<=2;i++)
+{cout<<a[i]<<"\t";
+}
+return 0;
+}
