a491578ed28c15f365e8141d6a84ca290ab845cc
Harsh Raj Jha <84229424+Hrjha30@users.noreply.github.com>
2021-10-19 14:19:24 +0530

    Create Area_of_equilateral_traingle.cpp

---
+++ Area_of_equilateral_traingle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{float side,area;
+cout<<"Enter the side : ";
+cin>>side;
+area=(sqrt(3)/4)*side*side;
+cout<<"Area is : "<<area;
+return 0;
+}
