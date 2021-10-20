217f4ca02ed9ebf1f112f695edd36fece964cb55
Harsh Raj Jha <84229424+Hrjha30@users.noreply.github.com>
2021-10-20 08:11:03 +0530

    Create Entering_and_displaying_the_string.cpp

---
+++ Entering_and_displaying_the_string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+int main()
+{char str[20];
+printf("Enter the word : "); 
+scanf("%s",&str);
+printf("\nName = %s",str); // will print only the first word until the space is given
+return 0;
+}
